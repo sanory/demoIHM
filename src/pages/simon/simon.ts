c88d83b769eba3f2eee40047f9a1ff1a137924c5
Arnaud <arnaud-av@live.fr>
2018-12-05 11:03:38 +0100

    Enigme code couleur v1

--- src/pages/simon/simon.ts
+++ src/pages/simon/simon.ts
@@ -8,12 +8,67 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+enum Color {
+  Blue,
+  Green,
+  Red,
+  Yellow
+}
+
 @IonicPage()
 @Component({
   selector: 'page-simon',
   templateUrl: 'simon.html',
 })
 export class SimonPage {
+  success : boolean = false;
+  fail : boolean = false;
+  sequence : Array<Color> = new Array();
+  correctSequence : Array<Color> = [Color.Blue, Color.Green, Color.Green, Color.Red]
+
+  addToSequence(col : number){
+    this.sequence.push(col);
+    this.readSequence();
+    if(this.fail || this.success){
+      this.success = false;
+      this.fail = false;
+    }
+    if (this.sequence.length >= 4){
+      this.checkSequence();
+    }
+  }
+
+  readSequence(){
+    let result :string = "";
+    this.sequence.forEach((col : Color) => {
+            result = result + col;
+    });
+    console.log(result);
+  }
+
+  checkSequence(){
+    let equals:boolean = false;
+    if(this.sequence.length == this.correctSequence.length){
+      equals = true;
+      for(let i=0; i<this.sequence.length; i++){
+        if(this.sequence[i] != this.correctSequence[i]){
+          equals = false;
+          break;
+        }
+      }
+    }
+
+    if(equals){
+      this.success = true;
+      this.fail = false;
+      this.sequence = [];
+    }
+    else{
+      this.success = false;
+      this.fail = true;
+      this.sequence = [];
+    }
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
